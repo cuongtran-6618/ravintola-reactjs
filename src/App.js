1162fee61e0edf256890954db2944f067b0cdc2b
Cuong Tran <mailtocuong@gmail.com>
2023-11-11 22:34:50 +0200

    Form controll

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 import DessertsList from "./DessertsList";
-
+import { useState } from "react";
 const desserts = [
   {
     name: "Chocolate Cake",
@@ -27,9 +27,38 @@ const desserts = [
 ];
 
 function App() {
+  const [start, setStart] = useState("5");
+  const [comments, setComments] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (start <= 2 || comments === undefined || comments === "") {
+      alert("why?");
+      return null;
+    }
+
+    console.log("formSubmit success");
+  };
+
   return (
     <div className="App">
       <DessertsList data={desserts} />
+      <form onSubmit={onSubmit}>
+        <label htmlFor="start">Star {start} </label>
+        <input
+          id="start"
+          type="range"
+          min="0"
+          max="5"
+          onChange={(e) => setStart(e.target.value)}
+        />
+        <label htmlFor="comment">Comment</label>
+        <textarea
+          id="comment"
+          onChange={(e) => setComments(e.target.value)}
+        ></textarea>
+        <button type="submit">Submit form</button>
+      </form>
     </div>
   );
 }
