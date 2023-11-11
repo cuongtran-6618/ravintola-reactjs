a7d514fbe804ec7baf50a9a800d5e8a098e7a40a
Cuong Tran <mailtocuong@gmail.com>
2023-11-11 19:41:32 +0200

    init commit

---
+++ src/DessertsList.js
@@ -0,0 +1,24 @@
+function DessertsList(props) {
+  let data = props.data.filter((item) => item.calories < 500);
+  
+  data.sort((a,b) => {
+    const aCalory = a.calories;
+    const bCalory = b.calories;
+    return aCalory === bCalory ? 0 : aCalory > bCalory ? 1 : -1;
+  });
+
+  const desertList = data.map((item) => {
+    const description = `${item.name} - ${item.calories} cal.`
+    return <li key={item.calories}>{item.name} {item.calories} cal.</li>
+  });
+
+  return (
+    <div>
+      <ul>
+        {desertList}
+      </ul>
+    </div>
+  )
+}
+
+export default DessertsList;
